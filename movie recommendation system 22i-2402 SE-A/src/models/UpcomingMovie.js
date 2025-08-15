bd5c6fed8773f6c2259d53016f1ab91b42f0f202
Eshal Omar <eshalomar951@gmail.com>
2025-08-15 21:23:40 +0500

    Initial commit

--- movie recommendation system 22i-2402 SE-A/src/models/UpcomingMovie.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const upcomingMovieSchema = new mongoose.Schema({
-    title: { type: String, required: true },
-    releaseDate: { type: Date, required: true },
-    reminders: [{ type: mongoose.Schema.Types.ObjectId, ref: 'User' }], // Reference to User model
-});
-
-module.exports = mongoose.model('UpcomingMovie', upcomingMovieSchema);
